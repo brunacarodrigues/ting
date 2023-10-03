9c702a1457eb0962f954e25c968fe7da80c25bc0
Bruna Rodrigues <brunacris.rodrigues@gmail.com>
2023-10-03 18:05:30 -0300

    task 03

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,23 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    if any(item['nome_do_arquivo'] == path_file for item in instance.values):
+        print(f'O arquivo {path_file} já foi processado anteriormente.')
+        return
+
+    linhas = txt_importer(path_file)
+
+    data_process = {
+        'nome_do_arquivo': path_file,
+        'qtd_linhas': len(linhas),
+        'linhas_do_arquivo': linhas
+    }
+
+    instance.enqueue(data_process)
+
+    return sys.stdout.write(f'{data_process}\n')
 
 
 def remove(instance):
