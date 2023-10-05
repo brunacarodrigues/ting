402f444c84a44a75b02a568be1bf0ef35ffd550e
Bruna Rodrigues <brunacris.rodrigues@gmail.com>
2023-10-05 13:13:49 -0300

    task 06

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,21 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    pq = PriorityQueue()
+    pq.enqueue({'qtd_linhas': 7})
+    assert len(pq.regular_priority.values) == 1
+
+    pq.dequeue()
+    assert len(pq.regular_priority.values) == 0
+    with pytest.raises(IndexError) as excinfo:
+        pq.search(1)
+    assert str(excinfo.value) == "Índice Inválido ou Inexistente"
+
+    pq.enqueue({'qtd_linhas': 10})
+    pq.enqueue({'qtd_linhas': 1})
+    assert len(pq.high_priority.values) == 1
+
+    pq.enqueue({'qtd_linhas': 2})
+    assert pq.search(1).get('qtd_linhas') == 2
