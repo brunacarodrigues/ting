ab5a6453160cfd22a4a35e6bc95edd461b405206
Bruna Rodrigues <brunacris.rodrigues@gmail.com>
2023-10-05 14:13:23 -0300

    task 07

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,24 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+
+    for value in range(len(instance)):
+
+        ocorrencias = []
+        pat = instance.search(value)
+
+    for index, linha in enumerate(pat['linhas_do_arquivo'], 1):
+        if word.lower() in linha.lower():
+            ocorrencias.append({'linha': index})
+
+    if ocorrencias:
+        result.append({
+            'palavra': word,
+            'arquivo': pat['nome_do_arquivo'],
+            'ocorrencias': ocorrencias
+        })
+
+    return result
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    """"Implemente a sua função aqui"""
